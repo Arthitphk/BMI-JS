d3a6df92706c1d7216f408799ad013865af630d6
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2026-01-04 21:36:56 +0700

    add stats of bmi

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ function calculateBMI() {
     let w = parseInt(weight.value);
     let h = parseInt(height.value);
     // คำนวณ BMI
-    let bmi = w / ((h/100) * (h/100)).toFixed(2);
-
-    resultBmi.innerText =`${bmi}`
+    let bmi = w/((h/100)*(h/100));
+    let bmi_result = bmi.toFixed(2);
+    //  แสดงหน้าจอ
+    resultBmi.innerText =`${bmi_result}`
+    // แสดงสถานะ
+    if (bmi_result < 18.5) {
+        resultStatus.innerText = 'ผอมเกินไป'
+    } else if (bmi_result <= 24.0) {
+        resultStatus.innerText = 'น้ำหนักปกติ เหมาะสม' 
+    } else if (bmi_result >= 29.9) {
+        resultStatus.innerText = 'อ้วน' 
+    } else if (bmi_result > 30.0) {
+        resultStatus.innerText = 'อ้วนมาก' 
+    }       
     
 }
 
